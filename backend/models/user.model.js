b2d4e46e39c7cd812965e6f6a92b7e9a658327b5
sainRaghuveer <raghuveersain987@gmail.com>
2023-06-03 17:31:21 +0530

    user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required:true
+    },
+    email: {
+        type: String,
+        required:true,
+        unique:true
+    },
+    password: {
+        type: String,
+        required:true
+    },
+    address: {
+        street: {
+            type: String
+        },
+        city: {
+            type: String
+        },
+        state: {
+            type: String
+        },
+        country: {
+            type: String
+        },
+        zip: {
+            type: String
+        }
+    }
+}, {timestamps:true});
+
+const userMOdel = mongoose.model("user", userSchema);
+
+module.exports={
+    userMOdel
+}
