15f6877cccca841609d60aaccf8d6993820ef9f4
sainRaghuveer <raghuveersain987@gmail.com>
2023-06-04 22:04:20 +0530

    user authentication working fine

--- backend/routes/restaurant.route.js
+++ backend/routes/restaurant.route.js
@@ -5,7 +5,12 @@ const restaurantRouter = express.Router();
 
 restaurantRouter.get("/restaurant", (req, res)=>{
     res.send("authentication is working fine");
-})
+});
+
+restaurantRouter.post("/restaurant", (req, res)=>{
+    res.send("authentication is working fine");
+});
+
 
 
 module.exports={
