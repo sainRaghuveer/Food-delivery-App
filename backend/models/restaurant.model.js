daf65b65f3097c91fddfdd9e2985e72351a91bc9
sainRaghuveer <raghuveersain987@gmail.com>
2023-06-05 13:18:45 +0530

    order model

--- backend/models/restaurant.model.js
+++ backend/models/restaurant.model.js
@@ -1,2 +1,47 @@
 const mongoose = require('mongoose');
 
+const restaurantSchema = new mongoose.Schema({
+    name: {
+        type:String
+    },
+    address: {
+        type:Object,
+        street: {
+            type: String
+        },
+        city: {
+            type: String
+        },
+        state: {
+            type: String
+        },
+        country: {
+            type: String
+        },
+        zip: {
+            type: String
+        }
+    },
+    menu: [{
+        // _id: ObjectId,
+        name: {
+            type: String
+        },
+        description: {
+            type: String
+        },
+        price: {
+            type:Number
+        },
+        image: {
+            type:String
+        }
+    }]
+});
+
+const restaurantModel = mongoose.model("restaurant", restaurantSchema);
+
+
+module.exports={
+    restaurantModel
+}
